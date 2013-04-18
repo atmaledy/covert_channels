c002081ca0cc1b81adb169fb303fb2491cd8b56b
Andrew Maledy <atmaledy@gmail.com>
2013-04-17 21:50:11 -0700

    Fixed errors and now packets send. Need to fix up the getopt as well as auto discover ip

--- C/client.c
+++ C/client.c
@@ -13,13 +13,19 @@
 #include <getopt.h>			//library for getting command line arguments..great tool!
 #include <string.h> //memset
 #include <sys/socket.h>    //for socket ofcourse
+#include <sys/ioctl.h> //for auto grabbing interface's ip
+#include <net/if.h>
+
 #include "tcp.h" //tcp header
 #include "ip.h" //ip header
 #include "client.h"
+
 #define TH_SIN 0x06
+
 void print_usage()
 {
     printf("Usage: ./a.out -s <server ip> -c <client_ip> -p <port>\n");
+    exit(1);
 }
 
 int main(int argc, char *argv[])
@@ -27,7 +33,7 @@ int main(int argc, char *argv[])
 
 	
 	int c, long_index=0; //for getopt
-	char* server_ip, *client_ip, *filename; // client collected data
+	char *server_ip, *client_ip, *filename, *iface; // client collected data
 	char buffer[PCKT_LEN];
 	int sport = 0;
 	int dport = 80;
@@ -37,35 +43,46 @@ int main(int argc, char *argv[])
 	//int long_index=0;
     static struct option long_options[] = {
         {"server-ip",      required_argument, 0,  's' },
-        {"client-ip",      required_argument, 0,  'c' },
+        {"client-ip",      optional_argument, 0,  'c' },
         {"sport",      required_argument, 0,  'p' },
-        {"dport",      required_argument, 0,  'd' },
+        {"dport",      required_argument, 0,  'q' },
+        {"iface",      optional_argument, 0,  'i' },
+        {"filename",      required_argument, 0,  'f' }
 
     };
-
-	while ((c = getopt_long (argc, argv, "s:c:p:d:f:",long_options, &long_index)) != -1)
+	while ((c = getopt_long (argc, argv, "s:c:p:d:f:i:",long_options, &long_index)) != -1)
 	         switch (c)
 	           {
-	           case 's':
-	             server_ip = optarg;
-	             break;
-	           case 'c':
-	             client_ip = optarg;
-	             break;
-	           case 'p':
-	             sport = atoi(optarg);
-	             break;
-	           case 'f':
-	             filename = optarg;
-	             break;
-	           case 'd':
-	           	  dport = atoi(optarg);
-	           	  break;
-	           default:
-	           	print_usage();
-	            exit(EXIT_FAILURE);
+               case 's':
+                   server_ip = optarg;
+                   break;
+               case 'c':
+                   client_ip = optarg;
+                   break;
+               case 'p':
+                   sport = atoi(optarg);
+                   break;
+               case 'f':
+                   filename = optarg;
+                   break;
+               case 'd':
+                   dport = atoi(optarg);
+                   break;
+               case 'i':
+                    iface = optarg; 
+                    break;
+               case ':':
+                    print_usage();
+                    break;
+               case '?':
+                    print_usage();
+                    break;
+               default:
+                    print_usage();
+                    break;
 	           }	           
-	           if(( server_ip == NULL) || (client_ip == NULL ))
+	           
+               if(( server_ip == NULL) || (client_ip == NULL ))
 	           {
 	           	print_usage();
 	           	exit(EXIT_FAILURE);
@@ -73,7 +90,32 @@ int main(int argc, char *argv[])
 	if(sport == 0)
 		sport = 1+(int)(10000.0*rand()/(RAND_MAX+1.0));
 	
-	printf("Source IP: %s, Destintation IP: %s, Dest port: %d, Source. port: %d \n", server_ip, client_ip, dport, sport);
+    /*
+    * If client_ip wasnt supplied, let's just figure it out...
+    */
+    if(client_ip == NULL)
+    {
+
+        if(iface ==  NULL) //did they give us an interface instead? if not...we'll take a gues
+        {
+            char *d_if = "en1";
+            iface = d_if;
+
+        }
+        char choice;
+        strcpy(client_ip, get_if_ip(iface));
+        printf("IP (%s) found on Interface %s.", client_ip, iface);
+        printf("This could be bad... are you sure you want to continue?");        
+        fgets(&choice, sizeof(choice), stdin);
+        if(!choice == 'y')
+        {
+            printf("Quitting...\n");
+            exit(EXIT_SUCCESS);          
+        }
+
+    }
+    printf("%s\n",iface);
+	printf("Server IP: %s, Client IP: %s, Dest port: %d, Source. port: %d \n", server_ip, client_ip, dport, sport);
 
 	//create the socket which we'll be writing to.	
 	sfd = socket(PF_INET, SOCK_RAW, IPPROTO_TCP);	
@@ -90,6 +132,8 @@ int main(int argc, char *argv[])
     //TCP header
     struct tcphdr *tcph = (struct tcphdr *) (packet + sizeof (struct ip)); //after the iphdr comes the tcphdr
 
+    fill_iphdr(iph, client_ip, server_ip);
+    fill_tcphdr(tcph, sport, dport);
     struct sockaddr_in sin; 
     struct pseudo_header pseudohdr;
  	
@@ -127,7 +171,8 @@ int main(int argc, char *argv[])
 	else while((ch=fgetc(file)) !=EOF)
 	//file opened...look through each character and send the packet
 	{
-		iph->ip_id = ch; //the payload hidden in the id field
+		
+        iph->ip_id = ch; //the payload hidden in the id field
 		tcph->th_seq = 1+(int)(10000.0*rand()/(RAND_MAX+1.0));
 		if (sendto (sfd, &packet, iph->ip_len ,  0, (struct sockaddr *) &sin, sizeof (sin)) < 0)
         {
@@ -136,7 +181,7 @@ int main(int argc, char *argv[])
         //Data send successfully
         else
         {
-            printf ("Packet Send. Character : %c \n" , iph->ip_id);
+            //printf ("Packet Send. Character : %c" , iph->ip_id);
         }
 	}
 
@@ -157,19 +202,19 @@ void fill_iphdr(struct ip* iph, char* source_ip, char* dest_ip)
     iph->ip_ttl = 255;
     iph->ip_p = IPPROTO_TCP;
     iph->ip_sum = 0;      //Set to 0 before calculating checksum
-    iph->ip_src = inet_addr(source_ip);    //Spoof the source ip address
-    iph->ip_dst = inet_addr(dest_ip);
+    iph->ip_src =  inet_ntoa(source_ip);    //Spoof the source ip address
+    iph->ip_dst =  inet_ntoa(dest_ip);
      
 
 }
 /*
 * 	Fills in the a supplied TCP header
 */
-void fill_tcphdr(struct tcphdr* tcph, char* source_port, char* dest_port)
+void fill_tcphdr(struct tcphdr* tcph, int source_port, int dest_port)
 {
     //Fill in TCP Header
-    tcph->th_sport = htons(atoi(source_port));
-    tcph->th_dport = htons(atoi(dest_port));
+    tcph->th_sport = htons(source_port);
+    tcph->th_dport = htons(dest_port);
     tcph->th_seq = 0;
     tcph->th_ack = 0;
     tcph->th_off = 5;  //tcp header size
@@ -179,7 +224,30 @@ void fill_tcphdr(struct tcphdr* tcph, char* source_port, char* dest_port)
     tcph->th_urp = 0;
 
 }
+/*
+* Get the host's ip address (if a spoofed one isnt supplied)
+*/
+char * get_if_ip(char* iface)
+{
+ int fd;
+ struct ifreq ifr;
 
+ fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+ /* Get an IPv4 IP address */
+ ifr.ifr_addr.sa_family = AF_INET;
+
+ /* I want IP address attached to "eth0" */
+ strncpy(ifr.ifr_name, iface, IFNAMSIZ-1);
+
+ ioctl(fd, SIOCGIFADDR, &ifr);
+
+ close(fd);
+
+ /* display result */
+ return inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr);
+  
+}
 //
 // Calculate checksum
 //
